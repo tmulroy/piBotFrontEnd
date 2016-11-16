545d598031b813b530a73beb6f0350f2b77f206d
tom <thomasjohnmulroy@gmail.com>
2016-11-16 11:47:48 -0500

    add src directory which holds react components

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 const path = require('path');
 
 module.exports = {
- devtool: 'source-map',
- entry: path.join(__dirname, 'client', 'index.jsx'),
- output: {
-   path: path.join(__dirname, 'dist', 'js'),
-   filename: 'bundle.js',
- },
- module: {
-   loaders: [
-     {
-       test: /.jsx?$/,
-       loader: 'babel-loader',
-       exclude: /node_modules/,
-       query: {
-         presets: ['es2015', 'react'],
-       },
-     },
-   ],
- },
+  devtool: 'source-map',
+  entry: path.join(__dirname, 'src', 'index.jsx'),
+  devServer: {
+    contentBase: path.join(__dirname, 'src', 'static'),
+    inline: true,
+    port: 8080,
+  },
+  output: {
+    path: path.join(__dirname, 'src', 'static', 'js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
 };
 
