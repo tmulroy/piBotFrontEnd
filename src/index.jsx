6776757144bc6de30e17e2daefc07e30dab4761d
tom <thomasjohnmulroy@gmail.com>
2016-11-16 11:58:56 -0500

    render a message from index.jsx to test if working

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/App.jsx';
+
+ReactDOM.render(<div>hello from index.jsx</div>, document.querySelector('#root'));
