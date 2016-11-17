809e70f174c46bc1933fb40e32cf00616b76db40
tom <thomasjohnmulroy@gmail.com>
2016-11-17 10:35:21 -0500

    add constructor to App.jsx and bind publishPubnubMessage to App.jsx context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import JointControl from './JointControl.jsx';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.publishPubNubMessage = this.publishPubNubMessage.bind(this);
+  }
+
+  publishPubNubMessage() {
+    console.log('hello from publishPubNubMessage')
+  }
+
   render() {
     return (
       <div>
         <JointControl jointName="shoulder"/>
-        <JointControl jointName="elbow" />  
+        <JointControl jointName="elbow" />
       </div>
     )
   }
