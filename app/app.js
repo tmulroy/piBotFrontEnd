51e3b12df0a29d4a7cca75de44ff29dcb3f0d5a0
tom <thomasjohnmulroy@gmail.com>
2016-11-17 13:30:50 -0500

    require body-parser in app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 const express = require('express'),
       app = express(),
-      morgan = require('morgan');
+      morgan = require('morgan'),
+      bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
